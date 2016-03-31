7a46507a01197ab4a651512716aa8c8a001aa445
Tim Owen <timowen1968@gmail.com>
2016-03-31 07:14:51 +0200

    Initial Commit

---
+++ platform_www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/com.jp.plugin.facebook/www/facebook.js",
+        "id": "com.jp.plugin.facebook.Facebook_Plugin",
+        "pluginId": "com.jp.plugin.facebook",
+        "clobbers": [
+            "facebook"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
---
+++ platform_www/plugins/com.jp.plugin.facebook/www/facebook.js
@@ -0,0 +1,40 @@
+cordova.define("com.jp.plugin.facebook.Facebook_Plugin", function(require, exports, module) {
+var facebook = {
+    login: function(successCallback, errorCallback) {
+		cordova.exec(successCallback,
+		            errorCallback,
+		            "Facebook_Plugin",
+		            "login",
+		            [{"file_path": "","message_text":""}]
+                );
+    	},
+   	logout: function(successCallback, errorCallback) {
+				cordova.exec( successCallback,
+				            errorCallback,
+							"Facebook_Plugin",
+							"logout",
+		        			[{"file_path": "","message_text":""}]
+		        		);
+	    },
+	share: function(successCallback, errorCallback, file_path, message_text) {
+				cordova.exec( successCallback,
+				            errorCallback,
+							"Facebook_Plugin",
+							"share",
+							[{"file_path": file_path,"message_text":message_text}]
+		        		);
+    },
+    check_loggedin: function(successCallback, errorCallback) {
+					cordova.exec( successCallback,
+					            errorCallback,
+								"Facebook_Plugin",
+								"check_logged_in",
+								[{"file_path": "","message_text":""}]
+			        		);
+    }
+}
+
+module.exports = facebook;
+
+
+});
